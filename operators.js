3d96b42d8f9fc799d7330fac5a506c822d5fb8a6
vandananukalagudam <vandana.n2001@gmail.com>
2022-03-09 20:21:04 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,34 @@
+console.log("hii");
+var a="hello";
+console.log(typeof a);
+console.log(a);
+var a=10;
+console.log(typeof a);
+console.log(a);
+
+// console.log(b);
+var c=true;
+console.log(typeof c);
+var a=10,b=30,c="20",d="10";
+console.log(a+b);
+console.log(a+b+c);
+console.log(a+c);
+console.log(a-c);
+console.log(a+c);
+console.log(a-c);
+console.log(a*c);
+console.log(a==c);
+console.log(a===c);
+console.log(a===b);
+console.log(a<c);
+console.log(d+a+b);
+console.log(a+b+d);
+var a="som",b="ads";
+console.log(a===b);
+var a=[2,3,4,5];
+console.log(typeof a);
+console.log(a[2]);
+console.log(a);
+
+
+
